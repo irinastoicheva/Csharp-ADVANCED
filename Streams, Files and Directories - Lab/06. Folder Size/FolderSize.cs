5f796597a4b71212b9ea3c7af64ec47d5040946a
irinastoicheva <irinastoicheva@mail.bg>
2019-01-29 15:47:55 +0200

    commit

--- Streams, Files and Directories - Lab/06. Folder Size/FolderSize.cs
+++ Streams, Files and Directories - Lab/06. Folder Size/FolderSize.cs
@@ -7,7 +7,19 @@
     {
         public static void Main()
         {
-            
+            string[] files = Directory.GetFiles("TestFolder");
+
+            long sum = 0;
+            int count = 0;
+
+            foreach (var file in files)
+            {
+                count++;
+                FileInfo fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            File.WriteAllText("TestFolder/Output.txt", ((double)sum / 1024 / 1024).ToString());
         }
     }
 }
