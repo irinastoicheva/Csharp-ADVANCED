cdcce30afca8355ca00c114fb83316b38aea56ab
irinastoicheva <irinastoicheva@mail.bg>
2019-01-29 12:57:27 +0200

    commit

---
+++ Streams, Files and Directories - Lab/06. Folder Size/FolderSize.cs
@@ -0,0 +1,13 @@
+﻿namespace _06._Folder_Size
+{
+    using System;
+    using System.IO;
+
+    class FolderSize
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
--- Streams, Files and Directories - Lab/5. Slice File/SliceFile.cs
+++ Streams, Files and Directories - Lab/5. Slice File/SliceFile.cs
@@ -1,12 +1,47 @@
 ﻿namespace _05._Slice_File
 {
     using System;
+    using System.IO;
+    using System.Text;
 
     class SliceFile
     {
         public static void Main()
         {
+            int parts = int.Parse(Console.ReadLine());
 
+            using (FileStream reader = new FileStream(@"Resources\05. Slice File\sliceMe.txt", FileMode.Open))
+            {
+                int sizesOfNewFiles = (int)Math.Ceiling(reader.Length / (double)parts);
+                byte[] buffer = new byte[1024];
+
+                for (int i = 1; i <= parts; i++)
+                {
+                    int currentRead = 0;
+
+                    using (FileStream writer = new FileStream($"Par-{i}.txt", FileMode.Create))
+                    {
+                        while (true)
+                        {
+                            int line = reader.Read(buffer, 0, buffer.Length);
+
+                            currentRead += line;
+
+                            if (line == 0)
+                            {
+                                break;
+                            }
+
+                            writer.Write(buffer, 0, line);
+
+                            if (currentRead >= sizesOfNewFiles)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
