ec798f7e4b6840e4f1deada6e6e1eb346fed0c2e
irinastoicheva <irinastoicheva@mail.bg>
2019-01-30 01:18:05 +0200

    commit

---
+++ 04. Streams, Files and Directories - Exersices/02.LineNumbers/LineNumbers.cs
@@ -0,0 +1,54 @@
+﻿namespace _02.LineNumbers
+{
+    using System;
+    using System.Linq;
+    using System.IO;
+
+    class LineNumbers
+    {
+        public static void Main()
+        {
+            using (StreamReader reader = new StreamReader(@"Resources/TextInput.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("Output.txt"))
+                {
+                    int counter = 1;
+
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        int symbolCount = 0;
+                        int punctuationMarksCount = 0;
+
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            char symbol = line[i];
+
+                            if (symbol == ' ')
+                            {
+
+                            }
+                            else if ((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z'))
+                            {
+                                symbolCount++;
+                            }
+                            else
+                            {
+                                punctuationMarksCount++;
+                            }
+                        }
+
+                        writer.WriteLine($"Line {counter}: {line} ({symbolCount})({punctuationMarksCount})");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
