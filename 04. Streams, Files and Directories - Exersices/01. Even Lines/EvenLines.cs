ba9c7ca107c66852b640873b9cf4496105b1ba6f
irinastoicheva <irinastoicheva@mail.bg>
2019-01-29 17:34:42 +0200

    commit

---
+++ 04. Streams, Files and Directories - Exersices/01. Even Lines/EvenLines.cs
@@ -0,0 +1,58 @@
+﻿namespace _01._Even_Lines
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    class EvenLines
+    {
+        public static void Main()
+        {
+            using (StreamReader reader = new StreamReader(@"Resource/Text.txt"))
+            {
+                int counter = 0;
+
+                while (true)
+                {
+                    string readLine = reader.ReadLine();
+
+                    if (readLine == null)
+                    {
+                        break;
+                    }
+
+                    List<string> words = new List<string>();
+                    StringBuilder sb = new StringBuilder();
+
+                    if (counter % 2 == 0)
+                    {
+                        for (int i = 0; i < readLine.Length; i++)
+                        {
+                            if (readLine[i] == '.' || readLine[i] == ',' || readLine[i] == '-' || readLine[i] == '!' || readLine[i] == '?')
+                            {
+                                sb.Append("@");
+                            }
+                            else if (readLine[i] == ' ')
+                            {
+                                words.Add(sb.ToString());
+                                sb.Clear();
+                            }
+                            else
+                            {
+                                sb.Append(readLine[i]);
+                            }
+                        }
+                        words.Add(sb.ToString());
+
+                        words.Reverse();
+                        Console.WriteLine(string.Join(" ", words));
+                    }
+
+                    counter++;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Csharp-ADVANCED
 
-1. Stacks and Queues - Lab
-2. Stacks and Queues - Exercise
-3. Multidimensional Arrays - Lab
-4. Multidimensional Arrays - Exercise
+01. Stacks and Queues - Lab
+01. Stacks and Queues - Exercise
+02. Multidimensional Arrays - Lab
+02. Multidimensional Arrays - Exercise
+03. Sets and Dictionaries Advanced - Lab
+03. Sets and Dictionaries Advanced - Exercise
+04. Streams, Files and Directories - Lab
 
 
