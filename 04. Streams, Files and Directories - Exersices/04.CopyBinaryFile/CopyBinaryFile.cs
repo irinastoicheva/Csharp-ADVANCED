e7ed918ea99c4027b857b086edc31fe36b2aac9b
irinastoicheva <irinastoicheva@mail.bg>
2019-01-30 12:05:06 +0200

    commit

---
+++ 04. Streams, Files and Directories - Exersices/04.CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,31 @@
+﻿namespace _04.CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    class CopyBinaryFile
+    {
+        public static void Main()
+        {
+            using (FileStream reader = new FileStream(@"Resources/copyMe.png", FileMode.Open))
+            {
+                byte[] buffer = new byte[1024*4];
+
+                using (FileStream writer = new FileStream("Output.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        int line = reader.Read(buffer, 0, buffer.Length);
+
+                        if (line == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, line);
+                    }
+                }
+            }
+        }
+    }
+}
