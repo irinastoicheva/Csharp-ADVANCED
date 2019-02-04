671cd5fbb8c9b6194d94e905780c3a8fc3b813f4
irinastoicheva <irinastoicheva@mail.bg>
2019-02-04 23:09:36 +0200

    commit

---
+++ 06. Defining Classes - Exercise/03.OldestFamilyMember/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                Person person = new Person(input[0], int.Parse(input[1]));
+                family.AddMember(person);
+            }
+
+            Person oldestMember = family.GetOldestMember();
+            Console.WriteLine(oldestMember);
+        }
+    }
+}
+
