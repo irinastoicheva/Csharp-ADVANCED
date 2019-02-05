24dd891c17153d72ab2785d9da62575db74ab30c
irinastoicheva <irinastoicheva@mail.bg>
2019-02-06 00:24:38 +0200

    commit

--- 06. Defining Classes - Exercise/09.RectangleIntersection/StartUp.cs
+++ 06. Defining Classes - Exercise/09.RectangleIntersection/StartUp.cs
@@ -2,19 +2,44 @@
 {
     using System;
     using System.Linq;
+    using System.Collections.Generic;
 
     public class StartUp
     {
         public static void Main()
         {
-            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            int numberOfRectangle = input[0];
-            int numberOfIntersectionChecks = input[1];
+            var firstLine = Console.ReadLine().Split();
 
-            for (int i = 0; i < numberOfRectangle; i++)
+            var rectangles = SetRectangles(int.Parse(firstLine[0]));
+            CheckIntersections(int.Parse(firstLine[1]), rectangles);
+        }
+
+        private static void CheckIntersections(int numberOfIntersectionChecks, Queue<Rectangle> rectangles)
+        {
+            while (numberOfIntersectionChecks > 0)
             {
+                var pair = Console.ReadLine().Split();
+                var firstRect = rectangles.Where(r => r.Id == pair[0]).FirstOrDefault();
+                var secondRect = rectangles.Where(r => r.Id == pair[1]).FirstOrDefault();
 
+                Console.WriteLine(firstRect.CheckIfTheyCross(secondRect) ? "true" : "false");
+
+                numberOfIntersectionChecks--;
             }
+        }
+
+        private static Queue<Rectangle> SetRectangles(int numberOfRectangles)
+        {
+            var rectangles = new Queue<Rectangle>(numberOfRectangles);
+
+            while (rectangles.Count < numberOfRectangles)
+            {
+                var input = Console.ReadLine().Split();
+                rectangles.Enqueue(new Rectangle(input[0], double.Parse(input[1]),
+                    double.Parse(input[2]), double.Parse(input[3]), double.Parse(input[4])));
+            }
+
+            return rectangles;
         }
     }
 }
