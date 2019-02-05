3e948d8b650ab56cac53fcd2958ddf23b828cfe1
irinastoicheva <irinastoicheva@mail.bg>
2019-02-06 01:31:21 +0200

    commit

--- 06. Defining Classes - Exercise/09.RectangleIntersection/StartUp.cs
+++ 06. Defining Classes - Exercise/09.RectangleIntersection/StartUp.cs
@@ -8,38 +8,28 @@
     {
         public static void Main()
         {
-            var firstLine = Console.ReadLine().Split();
+            int[] counts = Console.ReadLine().Split(new char[0], StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Rectangle[] rectangles = new Rectangle[counts[0]];
 
-            var rectangles = SetRectangles(int.Parse(firstLine[0]));
-            CheckIntersections(int.Parse(firstLine[1]), rectangles);
-        }
-
-        private static void CheckIntersections(int numberOfIntersectionChecks, Queue<Rectangle> rectangles)
-        {
-            while (numberOfIntersectionChecks > 0)
+            for (int i = 0; i < counts[0]; i++)
             {
-                var pair = Console.ReadLine().Split();
-                var firstRect = rectangles.Where(r => r.Id == pair[0]).FirstOrDefault();
-                var secondRect = rectangles.Where(r => r.Id == pair[1]).FirstOrDefault();
+                string[] tokens = Console.ReadLine().Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                string id = tokens[0];
+                double width = double.Parse(tokens[1]);
+                double height = double.Parse(tokens[2]);
+                double x = double.Parse(tokens[3]);
+                double y = double.Parse(tokens[4]);
 
-                Console.WriteLine(firstRect.CheckIfTheyCross(secondRect) ? "true" : "false");
-
-                numberOfIntersectionChecks--;
-            }
-        }
-
-        private static Queue<Rectangle> SetRectangles(int numberOfRectangles)
-        {
-            var rectangles = new Queue<Rectangle>(numberOfRectangles);
-
-            while (rectangles.Count < numberOfRectangles)
-            {
-                var input = Console.ReadLine().Split();
-                rectangles.Enqueue(new Rectangle(input[0], double.Parse(input[1]),
-                    double.Parse(input[2]), double.Parse(input[3]), double.Parse(input[4])));
+                rectangles[i] = new Rectangle(id, width, height, x, y);
             }
 
-            return rectangles;
+            for (int i = 0; i < counts[1]; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                Rectangle rect1 = rectangles.Where(r => r.Id == tokens[0]).First();
+                Rectangle rect2 = rectangles.Where(r => r.Id == tokens[1]).First();
+                Console.WriteLine(rect1.CheckIfTheyCross(rect2));
+            }
         }
     }
 }
