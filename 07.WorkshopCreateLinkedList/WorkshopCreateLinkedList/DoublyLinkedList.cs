c7a94603b32e0b57c7f4017fbac0ca891d327592
irinastoicheva <irinastoicheva@mail.bg>
2019-02-06 14:33:50 +0200

    commit

---
+++ 07.WorkshopCreateLinkedList/WorkshopCreateLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,143 @@
+﻿namespace WorkshopCreateLinkedList
+{
+    using System;
+
+    public class DoublyLinkedList
+    {
+        private class ListNode
+        {
+            public object Value { get; set; }
+
+            public ListNode Next { get; set; }
+
+            public ListNode Previous { get; set; }
+
+            public ListNode(object value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private ListNode head;
+        private ListNode tail;
+        public int Count { get; set; }
+
+        public object Head
+        {
+            get
+            {
+                IsValidateLinkedList();
+                return this.head.Value;
+            }
+        }
+
+        public object Tail
+        {
+            get
+            {
+                IsValidateLinkedList();
+                return this.tail.Value;
+            }
+        }
+
+        public void AddHead(object element)
+        {
+            ListNode newNode = new ListNode(element);
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newNode;
+            }
+            else
+            {
+                newNode.Next = this.head;
+                this.head.Previous = newNode;
+                this.head = newNode;
+            }
+
+            this.Count++;
+        }
+
+        public void AddTail(object element)
+        {
+            ListNode newNode = new ListNode(element);
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = this.tail;
+                this.tail.Next = newNode;
+                this.tail = newNode;
+            }
+
+            this.Count++;
+        }
+
+        public object RemoveHead()
+        {
+            IsValidateLinkedList();
+
+            ListNode first = this.head;
+            if (this.head == this.tail)
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else
+            {
+                ListNode newHead = new ListNode(this.head.Next.Value);
+                newHead.Previous = null;
+                this.head.Next = null;
+                this.head = newHead;
+            }
+
+            this.Count--;
+
+            return first.Value;
+        }
+
+        public object RemoveTail()
+        {
+            IsValidateLinkedList();
+
+            ListNode last = this.tail;
+            if (this.tail == this.head)
+            {
+                this.tail = null;
+                this.head = null;
+            }
+            else
+            {
+                ListNode newTail = new ListNode(this.tail.Previous.Value);
+                this.tail.Previous = null;
+                newTail.Next = null;
+                this.tail = newTail;
+            }
+
+            this.Count--;
+            return last.Value;
+        }
+
+        public object[] ToArrayLinkedList()
+        {
+            object[] arr = new object[this.Count];
+
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                arr[i] = this.head.Value;
+                this.head = this.head.Next;
+            }
+
+            return arr;
+        }
+
+        public void IsValidateLinkedList()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Doubly Linked List is Empty");
+            }
+        }
+    }
+}
---
+++ 07.WorkshopCreateLinkedList/WorkshopCreateLinkedList/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace WorkshopCreateLinkedList
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            DoublyLinkedList linkedList = new DoublyLinkedList();
+            linkedList.AddHead(1);
+            linkedList.AddHead(2);
+            linkedList.AddHead(3);
+            linkedList.AddHead(4);
+            linkedList.AddHead(5);
+
+            foreach (var item in linkedList.ToArrayLinkedList())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
