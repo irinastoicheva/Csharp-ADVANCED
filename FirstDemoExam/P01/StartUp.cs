087c17fc4c515815f934176901de67f1147d8cba
irinastoicheva <irinastoicheva@mail.bg>
2019-02-17 16:45:44 +0200

    commit

---
+++ FirstDemoExam/P01/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace P01
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<int> leftSocks = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            List<int> rightSocks = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            List<int> pair = new List<int>();
+
+            while (leftSocks.Count > 0 && rightSocks.Count > 0)
+            {
+                int leftSock = leftSocks.Last();
+                int rightSock = rightSocks.First();
+
+                if (leftSock > rightSock)
+                {
+                    pair.Add(leftSock + rightSock);
+                    leftSocks.RemoveAt(leftSocks.Count - 1);
+                    rightSocks.RemoveAt(0);
+                }
+                else if (leftSock == rightSock)
+                {
+                    leftSocks[leftSocks.Count - 1] += 1;
+                    rightSocks.RemoveAt(0);
+                }
+                else
+                {
+                    leftSocks.RemoveAt(leftSocks.Count - 1);
+                }
+            }
+
+            Console.WriteLine(pair.Max());
+
+            Console.WriteLine(string.Join(" ", pair));
+        }
+    }
+}
---
+++ FirstDemoExam/P02/StartUp.cs
@@ -0,0 +1,68 @@
+﻿namespace P02
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<string> headerRow = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            string[][] matrix = new string[number - 1][];
+
+            for (int i = 0; i < number - 1; i++)
+            {
+                string[] rowInput = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                matrix[i] = rowInput;
+            }
+           
+            string[] commandLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string command = commandLine[0];
+            string header = commandLine[1];
+            int index = headerRow.IndexOf(header);
+
+            switch (command)
+            {
+                case "filter":
+                    string value = commandLine[2];
+                    int rowIndex = -1;
+
+                    for (int i = 0; i < matrix.Length; i++)
+                    {
+                        if (matrix[i][index] == value)
+                        {
+                            rowIndex = i;
+                        }
+                    }
+                    Console.WriteLine(string.Join(" | ", headerRow));
+                    Console.WriteLine(string.Join(" | ", matrix[rowIndex]));
+                    
+                    break;
+
+                case "hide":
+
+                    headerRow.RemoveAt(index);
+                    Console.WriteLine(string.Join(" | ", headerRow));
+                    for (int i = 0; i < matrix.Length; i++)
+                    {
+                        Console.WriteLine(string.Join(" | ", matrix[i].Where(x => x != header)));
+                    }
+
+                    break;
+
+                case "sort":
+
+                    Console.WriteLine(string.Join(" | ", headerRow));
+                    foreach (var item in matrix.OrderBy(x => x[index]))
+                    {
+                        Console.WriteLine(string.Join(" | ", item));
+                    }
+                    break;
+            }
+        }
+    }
+}
